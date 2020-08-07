ff7186744dbc734b868adccb1726cdece6f94716
celin89 <celinbas@gmail.com>
2020-08-07 14:05:59 +0200

    calculer le prix total

--- CalculateTotalPrice.php
+++ CalculateTotalPrice.php
@@ -1,22 +1,23 @@
 <?php
 
 $discount= 10;
-$price= array(7,13,3,8,2,1);
+$price= array(7,13.5,3,8,2,1);
 calculateTotalPrice( $price , $discount);
 
 function calculateTotalPrice(array $price , $discount){
+   
  $sum = 0;
  rsort($price);
+ $rem = $discount/100 ;
  for($i=0; $i<count($price); $i++ )
      {
         $sum = $sum + $price[$i];
-        $rem = $discount/100 ;
-
         $total= $sum - $rem*$price[0];
      }
-     return  $total;
+      return floor($total);
 }
- 
+
+
 ?>
 
 
