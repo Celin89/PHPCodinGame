29372a2d55e53be8673768a65318859c7653d623
celin89 <celinbas@gmail.com>
2020-08-07 15:08:51 +0200

    Somme des elements du tableau

--- sommetab.php
+++ sommetab.php
@@ -1,4 +1,9 @@
 <?php
+$array = [0,1,2,3,4,5,3];
+$n1 = 0;
+$n2 = 8;
+calc($array, $n1, $n2);
+
 function calc($array, $n1, $n2){
 
  if ($n2 > count($array) || $n1 > $n2 || $n1 < 0){
@@ -10,11 +15,8 @@ for ($i=$n1 ; $i<=$n2 ; $i++)
 {
     $somme = $somme + $array[$i];         
 }
-echo $somme;
+return $somme;
 }
 }
 
-$array = [0,1,2,3,4,5,3];
-$n1 = 0;
-$n2 = 8;
-calc($array, $n1, $n2);
+
