8875b6243f930b3bdf99fc61b2951b143b8539e0
celin89 <celinbas@gmail.com>
2020-08-07 15:08:17 +0200

    Somme des elements du tableau

--- tableausomme.php
+++ tableausomme.php
@@ -1,5 +1,10 @@
 <?php
 
+$array = [1, 2, 3];
+$n1 = 0;
+$n2 = 0;
+calc($array, $n1, $n2);
+
 function calc($array, $n1, $n2)
 {
     if ($n2 <= count($array) && $n1 <= $n2 && $n1 >= 0)
@@ -11,11 +16,8 @@ function calc($array, $n1, $n2)
         $somme = $n1+$n2 ;
     }
 
-   echo $somme;
+   return $somme;
    
 }
 
-$array = [1, 2, 3];
-$n1 = 0;
-$n2 = 0;
-calc($array, $n1, $n2);
+
