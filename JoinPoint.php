d452ffdc3d3a6f909a5ae8770fec85aae16fb13d
celin89 <celinbas@gmail.com>
2020-08-07 14:42:56 +0200

    Trouver le point de jointure

--- JoinPoint.php
+++ JoinPoint.php
@@ -5,21 +5,20 @@ function simple($mNbr)
     $cumul=$mNbr;
     for ($i=0;$i<strlen($nbr); $i++) $cumul+=substr($nbr, $i,1);
     return $cumul;
-
 }
- 
 
 function computeJoinPoint($mS1,$mS2)
 {
-    $i=0;
-while(($mS1!=$mS2) && ($i<2000000))
-        {
-            $i++;
-            $mS1=simple($mS1);
-            $mS2=simple($mS2);
-            echo $mS1.'-'.$mS2 ;
-        }
+  $i=0;
+ while($mS1!=$mS2 && $i<2000000)
+     {
+        $i++;
+        $mS1=simple($mS1);
+        $mS2=simple($mS2);
+        return $mS1.'-'.$mS2 ;
+     }
 }
+
 $mS1=471;
 $mS2=480;
 computeJoinPoint($mS1,$mS2);
