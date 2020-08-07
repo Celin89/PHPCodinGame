6855fc6d12f6b47a3be5a988116865d7e6c4ff9d
celin89 <celinbas@gmail.com>
2020-08-07 14:53:48 +0200

    Trouver l'entier le plus proche de zero

--- computeClosestToZero.php
+++ computeClosestToZero.php
@@ -4,13 +4,12 @@ function computeClosestToZero(array $ts) {
   if (empty($ts)){
    return 0;
   }
-
   $temp=null;
   rsort($ts);
   foreach($ts as $key){
-    if(is_null($temp)|| $temp>abs($key)){
+    if( $temp>abs($key)||is_null($temp) )
+    {
       $temp!==$key ? $temp=$key :'';
-
     }
     
   }
