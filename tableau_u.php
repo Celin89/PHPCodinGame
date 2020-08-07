1693f55028b53c9bba03593f045403cf32beba87
celin89 <celinbas@gmail.com>
2020-08-07 15:05:17 +0200

    tableau unique

--- tableau_u.php
+++ tableau_u.php
@@ -1,4 +1,6 @@
 <?php
+$tVals = Array(5, 3, 5, 1, 8, 3, 5, 1, 3, 2, 2, 9, 1);
+ValeursUniques($tVals);
 function ValeursUniques($tableau) {
 
   // On récupère les mêmes valeurs dans 1 seule case
@@ -14,8 +16,7 @@ function ValeursUniques($tableau) {
   return($tUnique);
 }
 
-$tVals = Array(5, 3, 5, 1, 8, 3, 5, 1, 3, 2, 2, 9, 1);
-ValeursUniques($tVals);
+
 
 
 ?>
